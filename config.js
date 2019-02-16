f67891bef59b6843beeed85d7b10ad6435ed2261
Reex99 <47679520+Reex99@users.noreply.github.com>
2019-02-16 20:05:28 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NTQ2NDA1MjQzNDc1MTk3OTUy.D0nv7Q.S_uD9PR31DjY5LJv_QqOh4sILWA', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: '*-',
+    devs: ['352222161559093269']
+}
